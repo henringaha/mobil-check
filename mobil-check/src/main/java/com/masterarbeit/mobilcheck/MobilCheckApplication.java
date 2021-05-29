2f134ca01063ba1ec39a068693228bf7133fec6d
Dell <Dell@DESKTOP-1CC24V0>
2021-05-29 12:58:05 +0200

    Projekterstellung

---
+++ mobil-check/src/main/java/com/masterarbeit/mobilcheck/MobilCheckApplication.java
@@ -0,0 +1,13 @@
+package com.masterarbeit.mobilcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilCheckApplication.class, args);
+	}
+
+}
---
+++ mobil-check/src/test/java/com/masterarbeit/mobilcheck/MobilCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masterarbeit.mobilcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobilCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
